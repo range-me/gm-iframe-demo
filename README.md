dbffdfaba0a8fd507a53087b8c94e67b7ebf3c92
Frank <ans0600@gmail.com>
2022-01-28 14:26:06 +1100

    Update www domain handling

--- README.md
+++ README.md
@@ -5,8 +5,14 @@
 * Add the following to local host file
 ```
   127.0.0.1 rangeme-test.com
+  127.0.0.1 www.rangeme-test.com
+  127.0.0.1 rangeme-test2.com
+  127.0.0.1 rangeme-test3.com
   127.0.0.1 marketgate.com
 ```
 * yarn
-* yarn start #start the server
-* Visit http://rangeme-test.com:1234/ in the browser
+* yarn start
+* Visit http://rangeme-test.com:1234/ in the browser, the iframe should work correctly
+* Visit http://www.rangeme-test.com:1234/ in the browser, the iframe should work correctly
+* Visit http://rangeme-test2.com:1234/ in the browser, the iframe should work correctly
+* Visit http://rangeme-test3.com:1234/ in the browser, the iframe should NOT load
--- index.js
+++ index.js
@@ -7,7 +7,9 @@ app.get("/ecrm-meeting-registration", (req, res) => {
   res.set("Content-Type", "text/html")
   // current X-Frame-Options header should be removed and replaced with Content-Security-Policy
   // res.set("X-Frame-Options", "sameorigin")
-  res.set("Content-Security-Policy", "frame-ancestors http://rangeme-test.com:1234;")
+  // Production value for frame-ancestors should be "frame-ancestors https://app.rangeme.com https://*.range.me https://range.me;"
+  // Staging value for frame-ancestors should be "frame-ancestors https://testing.rangeme.com;"
+  res.set("Content-Security-Policy", "frame-ancestors http://rangeme-test.com:1234 http://*.rangeme-test.com:1234 http://rangeme-test2.com:1234;")
 
   res.sendFile(path.join(__dirname, "/children.html"));
 })
